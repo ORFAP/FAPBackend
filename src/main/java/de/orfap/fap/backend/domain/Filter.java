e54585e6a16aea579856054989acca7244684bca
René Zarwel <darenegade@gmx.de>
2016-04-06 19:27:33 +0200

    User+Filter

---
+++ src/main/java/de/orfap/fap/backend/domain/Filter.java
@@ -0,0 +1,44 @@
+package de.orfap.fap.backend.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.ElementCollection;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.validation.constraints.Past;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Organization: HM FK07.
+ * Project: FAPBackend, de.orfap.fap.backend.domain
+ * Author(s): Rene Zarwel
+ * Date: 06.04.16
+ * OS: MacOS 10.11
+ * Java-Version: 1.8
+ * System: 2,3 GHz Intel Core i7, 16 GB 1600 MHz DDR3
+ */
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Filter extends BaseEntity{
+
+    @ElementCollection
+    List<String> airports;
+
+    @ElementCollection
+    List<String> airlines;
+
+    @Past
+    Date start;
+
+    @Past
+    Date end;
+
+    @Embedded
+    User user;
+
+}
