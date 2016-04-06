929c62dbe5d2cf07def6d31d40817a239efba1bf
René Zarwel <darenegade@gmx.de>
2016-04-06 17:02:37 +0200

    Init

---
+++ src/main/java/de/orfap/fap/backend/FapBackendApplication.java
@@ -0,0 +1,12 @@
+package de.orfap.fap.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FapBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FapBackendApplication.class, args);
+	}
+}
